b57ed7ca64c346cf0bf7d253c73f603a9374fd19
sadia <sadiaripa81@gmail.com>
2025-08-01 20:59:16 +0600

    level order tree

---
+++ module 18/max_height.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root != NULL)
+    {
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *my_left, *my_right;
+
+        if (l == -1)
+            my_left = NULL;
+        else
+            my_left = new Node(l);
+
+        if (r == -1)
+            my_right = NULL;
+        else
+            my_right = new Node(r);
+
+        p->left = my_left;
+        p->right = my_right;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int max_height(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 0;
+    }
+    int l = max_height(root->left);
+    int r = max_height(root->right);
+    return max(l, r) +1;
+}
+int main()
+{
+    Node *root = input_tree();
+    cout << max_height(root);
+    return 0;
+}
