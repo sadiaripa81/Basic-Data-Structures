59af279079ac447f76731c653023c1e1dc0db62f
sadia <sadiaripa81@gmail.com>
2025-08-09 21:39:10 +0600

    module 21

---
+++ module 21/insert_node.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root != NULL)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *my_left, *my_right;
+
+        if (l == -1)
+            my_left = NULL;
+        else
+            my_left = new Node(l);
+
+        if (r == -1)
+            my_right = NULL;
+        else
+            my_right = new Node(r);
+
+        p->left = my_left;
+        p->right = my_right;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+void level_order(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+        cout << f->val << " ";
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+        {
+            q.push(f->right);
+        }
+    }
+}
+bool insert(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        root = new Node(val);
+    }
+    if (root->val > val)
+    {
+        if (root->left == NULL)
+        {
+            root->left = new Node(val);
+        }
+        else
+        {
+            insert(root->left, val);
+        }
+    }
+    else
+    {
+        if (root->right == NULL)
+        {
+            root->right = new Node(val);
+        }
+        else
+        {
+            insert(root->right, val);
+        }
+    }
+}
+int main()
+{
+    Node *root = input_tree();
+    int val;
+    cin >> val;
+    insert(root, val);
+    insert(root, 11);
+    level_order(root);
+    return 0;
+}
